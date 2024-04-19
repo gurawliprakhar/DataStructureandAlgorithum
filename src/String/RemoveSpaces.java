046ba3c2658d7670c4023294c508e894ef8d396a
Prakhar Tripathi <110407222+gurawliprakhar@users.noreply.github.com>
2024-04-19 19:53:30 +0530

    update remove.java

--- src/String/RemoveSpaces.java
+++ src/String/RemoveSpaces.java
@@ -18,10 +18,50 @@ public class RemoveSpaces  {
         }
         return count;
     }
+    static String toUpperCase(String s){
+        String t = "";
+        for(int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+            if(c >= 'a' && c<='z'){
+              t = t + (char)(c-32);
+            }else{
+                t = t + c;
+            }
+        }
+        return t;
+    }
+    static int indexOf(String s, char c) {
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == c) {
+                return i;
+            }
+        }
+        return -1; // Return -1 if the character is not found
+    }
+    static String trim(String s){
+       String t = "";
+       int si=0, ei=0;
+       for(int i=0; i<s.length(); i++){
+           if(s.charAt(i) != ' '){
+               si = i;
+               break;
+           }
+       }
+        for(int i=s.length()-1; i>=0; i--){
+            if(s.charAt(i) != ' '){
+                ei = i;
+                break;
+            }
+        }
+        for(int i=si; i<=ei; i++){
+              t += s.charAt(i);
+        }
+        return t;
+    }
 
     public static void main(String[] args) {
-        String s = "How are you";
-        String c = "o";
-        System.out.println(countCharInString(s,c));
+        String s = "   Haaw   ";
+        System.out.println(trim(s));
+        }
     }
-}
+
