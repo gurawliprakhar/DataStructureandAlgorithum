42e0f086e735ab12e4f139d633be51dc02ac6b52
Prakhar Tripathi <110407222+gurawliprakhar@users.noreply.github.com>
2024-05-04 09:51:42 +0530

    update remove.spaces.java

--- src/String/RemoveSpaces.java
+++ src/String/RemoveSpaces.java
@@ -1,5 +1,7 @@
 package String;
 
+import java.util.Arrays;
+
 public class RemoveSpaces  {
     static int countSpaces(String s){
         int count = 0;
@@ -101,11 +103,74 @@ public class RemoveSpaces  {
            }
        }
     }
-   
-
-    public static void main(String[] args) {
-        String st = "parul";
-        printAllSubstring(st);
+    static void nonRepSubstring(String s){
+        String res = "";
+        for (int i = 0; i < s.length(); i++) {
+            String temp ="";
+            for(int j=i; j<s.length(); j++){
+                if(temp.indexOf(s.charAt(i)) == -1){
+                    temp = temp + s.charAt(j);
+                }else{
+                    break;
+                }
+            }if(temp.length() > res.length()){
+                res = temp;
+            }
         }
+        System.out.println(res);
+    }
+    static boolean isAnagram(String s1, String s2){
+       String temp = "";
+       for(int i=0; i< s1.length(); i++){
+          if(s1.charAt(i) != ' '){
+             temp += s1.charAt(i);
+          }
+       }
+       s1 = temp;
+       temp="";
+        for(int i=0; i< s2.length(); i++){
+            if(s2.charAt(i) != ' '){
+                temp += s2.charAt(i);
+            }
+        }
+       s2 = temp;
+       s1 = s1.toLowerCase();
+       s2 = s2.toLowerCase();
+       char arr1[] = s1.toCharArray();
+       char arr2[] = s2.toCharArray();
+       Arrays.sort(arr1);
+       Arrays.sort(arr2);
+       s1 = new String(arr1);
+       s2 = new String(arr2);
+
+      return  s1.equals(s2);
+    }
+    static boolean isPanagram(String s){
+        String t = "abcdefghijklmnopqrsuvwxyz";
+        s = s.toLowerCase();
+        int count = 0;
+        for(int i=0; i<s.length(); i++){
+           if(s.indexOf(t.charAt(i)) >= 0) count++;
+           else break;
+        }
+        if(count == 26) return true;
+        else return false;
+    }
+    static int invalidBracket(String s){
+        int bracket=0, count=0;
+      for(int i=0; i<s.length(); i++){
+          char c = s.charAt(i);
+          if(c == '(') bracket++;
+          else{
+              if(bracket <= 0) count ++;
+              else bracket--;
+          }
+      }
+      return count+bracket;
+    }
+    public static void main(String[] args) {
+        String s = ")))(((())";
+        System.out.println(invalidBracket(s));
+       }
     }
 
