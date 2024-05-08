d1638aa6efdf28ba8f9af946c85eea4d14c14ace
Prakhar Tripathi <110407222+gurawliprakhar@users.noreply.github.com>
2024-05-08 10:28:08 +0530

    update

--- src/BitManupulation/PowOf2.java
+++ src/BitManupulation/PowOf2.java
@@ -10,8 +10,17 @@ public class PowOf2 {
          if((n&(n<<1)) != 0) return  true;
          else return false;
     }
+    
+    static int countConsecutive(int n){
+        int count = 0;
+        while(n>0){
+            n = (n&(n<<1));
+            count++;
+        }
+        return count;
+    }
     public static void main(String[] args) {
-        int n = 01100101;
-        System.out.println(conscutive1s(n));
+        int n = 0b01100101;
+        System.out.println(countConsecutive(n));
     }
 }
