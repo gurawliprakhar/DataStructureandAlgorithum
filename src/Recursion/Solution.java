81f8f3df21869b6f2b69bd9157107b77fb938109
Prakhar Tripathi <110407222+gurawliprakhar@users.noreply.github.com>
2024-05-11 12:36:56 +0530

    Update Soultion.java

--- src/Recursion/Solution.java
+++ src/Recursion/Solution.java
@@ -10,7 +10,36 @@ public class Solution {
         if(n==1 || n==2) return 1;
        return fibonacci(n-1) + fibonacci(n-2);
     }
+
+    static void nNaturalNo(int n){
+        if(n==0) return ;
+        nNaturalNo(n-1);
+        System.out.println(n);
+    }
+
+    static int countDigit(int n){
+        if(n==0) return 0;
+        return countDigit(n/10)+1;
+    }
+
+    static int sumOfDigit(int n){
+        if(n==0) return 0;
+         return sumOfDigit(n/10)+n%10;
+    }
+
+    static String reverseString(String s, String r, int i){
+        if(i < 0 ) return r;
+        return reverseString(s, r + s.charAt(i), i - 1);
+    }
+
+    static boolean isPalndrome(String s,int i, int j){
+       if(s.charAt(i) != s.charAt(j)) return false;
+       if(j <= i) return true;
+       return isPalndrome(s,i+1,j-1);
+    }
+
     public static void main(String[] args) {
-        System.out.print(fibonacci(6));
+        String s = "madam";
+        System.out.println(isPalndrome(s,0,s.length()-1));
     }
 }
